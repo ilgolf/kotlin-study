ef83f44efe62ccd6c63cbd3c620db59bd01ccd53
Developer.GOLF <ilgolc@naver.com>
2022-09-11 15:46:05 +0900

    Kotlin Lec07 - 코틀린에서 예외를 다루는 방법

---
+++ src/lec07/Lec07.kt
@@ -0,0 +1,18 @@
+package lec07
+
+fun parseIntOrThrow(str: String): Int {
+    try {
+        return str.toInt()
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException("주어진 $str 은 숫자가 아닙니다.")
+    }
+}
+
+// kotlin에서 try는 Expression
+fun parseIntOrThrowV2(str: String): Int? {
+    return try {
+        str.toInt()
+    } catch (e : NumberFormatException) {
+        null
+    }
+}
